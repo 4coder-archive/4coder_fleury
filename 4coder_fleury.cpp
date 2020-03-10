3e2dd01bd9cdf6584ca1acb3163e27764791b0e5
Ryan Fleury <ryan.j.fleury@gmail.com>
2020-03-10 16:32:11 -0600

    Update custom_layer_init for 4.1.4

--- 4coder_fleury.cpp
+++ 4coder_fleury.cpp
@@ -352,30 +352,16 @@ void
 custom_layer_init(Application_Links *app)
 {
     Thread_Context *tctx = get_thread_context(app);
-    
-    // NOTE(allen): setup for default framework
-    {
-        async_task_handler_init(app, &global_async_system);
-        
-        code_index_init();
-        buffer_modified_set_init();
-        Profile_Global_List *list = get_core_profile_list(app);
-        ProfileThreadName(tctx, list, string_u8_litexpr("main"));
-        initialize_managed_id_metadata(app);
-        set_default_color_scheme(app);
-    }
-    
-    // NOTE(allen): default hooks and command maps
-    {
-        set_all_default_hooks(app);
-        set_custom_hook(app, HookID_Tick,          Fleury4Tick);
-        set_custom_hook(app, HookID_RenderCaller,  Fleury4Render);
-        set_custom_hook(app, HookID_BeginBuffer,   Fleury4BeginBuffer);
-        set_custom_hook(app, HookID_Layout,        Fleury4Layout);
-        mapping_init(tctx, &framework_mapping);
-        Fleury4SetBindings(&framework_mapping);
-    }
-    
+ 
+    default_framework_init(app);
+    set_all_default_hooks(app);
+    set_custom_hook(app, HookID_Tick,          Fleury4Tick);
+    set_custom_hook(app, HookID_RenderCaller,  Fleury4Render);
+    set_custom_hook(app, HookID_BeginBuffer,   Fleury4BeginBuffer);
+    set_custom_hook(app, HookID_Layout,        Fleury4Layout);
+    mapping_init(tctx, &framework_mapping);
+    Fleury4SetBindings(&framework_mapping);
+
     // NOTE(rjf): Open calc buffer.
     {
         Buffer_ID calc_buffer = create_buffer(app, string_u8_litexpr("*calc*"),
